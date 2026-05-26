60027c86616b7d36151973356b21d30bfc9a09da
SillyProotSoda <103301261+ytsodacan@users.noreply.github.com>
2026-05-27 08:51:48 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ var _SYSTEM_PATHS = ["C:/Windows/System32/kernel32.dll", "/var/www/html/cine-os/
 var _devBuildVer = "3.0.1";
 
 var APPS = {
-    'minecraft': {title: 'Minecraft', path: 'script/Apps/Minecraft/index.html', icon: 'script/Apps/Minecraft/icon.png', pinned: true, windowed: true},
-    'games': {title: 'Game Library', path: 'script/Apps/Games/index.html', icon: 'script/Apps/Games/icon.png', pinned: true},
-    'web': {title: 'Browser', path: 'script/Apps/Web/index.html', icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Google_Chrome_icon_%28February_2022%29.svg/2048px-Google_Chrome_icon_%28February_2022%29.svg.png', pinned: true},
-    'spotify': {title: 'Spotify', path: 'script/Apps/Spotify/index.html', icon: 'https://cdn.pixabay.com/photo/2016/10/22/00/15/spotify-1759471_1280.jpg', pinned: true},
-    'settings': {title: 'Settings', internal: true, icon: 'https://cdn.iconscout.com/icon/free/png-256/free-apple-settings-icon-svg-download-png-493162.png', pinned: true},
+    'minecraft': {title: 'Minecraft', path: 'script/Apps/Minecraft/index.html', icon: 'script/Apps/Minecraft/icon.png', pinned: true, windowed: true, openMode: "iframe"   // or "newtab" or "redirect"
+},
+    'games': {title: 'Game Library', path: 'script/Apps/Games/index.html', icon: 'script/Apps/Games/icon.png', pinned: true, openMode: "newtab"   // or "newtab" or "redirect"},
+    'web': {title: 'Browser', path: 'script/Apps/Web/index.html', icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Google_Chrome_icon_%28February_2022%29.svg/2048px-Google_Chrome_icon_%28February_2022%29.svg.png', pinned: true, openMode: "iframe"   // or "newtab" or "redirect"
+},
+    'spotify': {title: 'Spotify', path: 'script/Apps/Spotify/index.html', icon: 'https://cdn.pixabay.com/photo/2016/10/22/00/15/spotify-1759471_1280.jpg', pinned: true, openMode: "iframe"   // or "newtab" or "redirect"
+},
+    'settings': {title: 'Settings', internal: true, icon: 'https://cdn.iconscout.com/icon/free/png-256/free-apple-settings-icon-svg-download-png-493162.png', pinned: true, openMode: "iframe"   // or "newtab" or "redirect"
+},
 };
 
 var savedPins = localStorage.getItem('silly_pins_v5');
@@ -512,6 +516,17 @@ function openWindow(id) {
 
     if(!win) {
         let dat = APPS[id] || {title: 'APP', path: 'about:blank'};
+        // Allow apps to bypass iframe
+if (dat.openMode === "redirect") {
+    window.location.href = dat.path;
+    return;
+}
+
+if (dat.openMode === "newtab") {
+    window.open(dat.path, "_blank");
+    return;
+}
+
         win = document.createElement('div');
         win.id = 'win-' + id;
         win.className = 'window active';
